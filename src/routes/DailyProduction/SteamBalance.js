f31133ae8ad5dab6a7d813b1367d3e2ebce078ce
hehuasa <754892110@qq.com>
2018-10-15 15:44:22 +0800

    del console

--- src/routes/DailyProduction/SteamBalance.js
+++ src/routes/DailyProduction/SteamBalance.js
@@ -1,8 +1,8 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'dva';
 import Scrollbars from 'react-custom-scrollbars';
 import moment from 'moment';
-import { Table, Select, DatePicker } from 'antd';
+import { Table, DatePicker } from 'antd';
 import Waterfall from './chart/Waterfall';
 import styles from './index.less';
 
@@ -142,7 +142,7 @@ export default class SteamBalance extends PureComponent {
                 pagination={false}
                 // loading={this.props.loading.global}
                 scroll={{ x: this.state.scrollX }}
-                rowClassName={(record, index) => {
+                rowClassName={(_, index) => {
                   return index % 2 === 0 ? styles.blue : styles.blueRow;
                 }}
                 bordered
