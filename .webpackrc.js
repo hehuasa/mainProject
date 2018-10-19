310b356786dd27ee41efaa3f5e453e7b18e2cd91
Holger <18990755409@163.com>
2018-10-19 08:33:20 +0800

    1.合并现场代码

--- .webpackrc.js
+++ .webpackrc.js
@@ -13,8 +13,7 @@ export default
   },
   "proxy": {
     "/emgc": {
-      // "target": "http://10.157.24.13:8080/",
-      "target": "http://192.168.0.6:8080/",
+      "target": "http://10.157.24.55:8080/",
       "changeOrigin": true,
       "pathRewrite": { "^/emgc": "" }
     },
