8bee2acd6cf6c616b8194d78ea1b82e5bbe6a225
hehuasa <754892110@qq.com>
2018-11-07 11:28:27 +0800

    1.地图搜索 2.搜索资源信息窗为undefined的问题 3.地图搜索结果按拼音排序

--- src/models/vocsMonitor.js
+++ src/models/vocsMonitor.js
@@ -1,5 +1,5 @@
-// import { response } from '../../mock/vocsData';
-import { vocsList } from '../services/api';
+import { response } from '../../mock/vocsData';
+// import { vocsList } from '../services/api';
 
 export default {
   namespace: 'vocsMonitor',
@@ -12,10 +12,13 @@ export default {
   effects: {
     // 获取列表
     * fetchList({ payload }, { call, put }) {
-      const response = yield call(vocsList, payload);
+      // const response = yield call(vocsList, payload);
+      console.log('response.data.length', response.data.length);
+      console.log('response.data', response.data);
+      const newData = response.data.filter(value => value.gisCode);
       yield put({
         type: 'queryList',
-        payload: response.data,
+        payload: newData,
       });
     },
   },
